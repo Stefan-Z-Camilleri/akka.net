91a90039dc15bc22b067b116798647600a5e19e8
ravengerUA <yurisikorsky@gmail.com>
2016-12-19 23:48:00 +0200

    fixed snapshot

--- src/core/Akka.Persistence/Snapshot/LocalSnapshotStore.cs
+++ src/core/Akka.Persistence/Snapshot/LocalSnapshotStore.cs
@@ -175,7 +175,7 @@ namespace Akka.Persistence.Snapshot
         protected FileInfo WithOutputStream(SnapshotMetadata metadata, Action<Stream> p)
         {
             var tmpFile = GetSnapshotFileForWrite(metadata, ".tmp");
-            WithStream(new FileStream(tmpFile.FullName, FileMode.Create), stream =>
+            WithStream(new BufferedStream(new FileStream(tmpFile.FullName, FileMode.Create)), stream =>
             {
                 p(stream);
                 stream.Flush();
@@ -186,7 +186,7 @@ namespace Akka.Persistence.Snapshot
 
         private T WithInputStream<T>(SnapshotMetadata metadata, Func<Stream, T> p)
         {
-            return WithStream(new FileStream(GetSnapshotFileForWrite(metadata).FullName, FileMode.Open), p);
+            return WithStream(new BufferedStream(new FileStream(GetSnapshotFileForWrite(metadata).FullName, FileMode.Open)), p);
         }
 
         private T WithStream<T>(Stream stream, Func<Stream, T> p)
